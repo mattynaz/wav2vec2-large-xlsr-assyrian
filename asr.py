84c79d0f855a5217c951f0ea2ba846cd2e7dc1ca
Matthew Nazari <matthewanazari@gmail.com>
2022-04-18 12:49:28 -0400

    tune training args

--- asr.py
+++ asr.py
@@ -34,10 +34,10 @@ model.gradient_checkpointing_enable()
 training_args = TrainingArguments(
       output_dir='./wav2vec2-nena',
       group_by_length=True,
-      per_device_train_batch_size=48,
-      gradient_accumulation_steps=1,
+      per_device_train_batch_size=16,
+      gradient_accumulation_steps=2,
       evaluation_strategy='steps',
-      num_train_epochs=80,
+      num_train_epochs=100,
       fp16=True,
       save_steps=1000,
       eval_steps=50,
