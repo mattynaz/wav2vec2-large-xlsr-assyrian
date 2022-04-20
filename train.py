58ce626fd90ee1c1ed1076c9c5ea24057d2d4b71
Matthew Nazari <matthewanazari@gmail.com>
2022-04-20 15:53:52 -0400

    decrease batch size

--- train.py
+++ train.py
@@ -27,16 +27,11 @@ processor = load_processor()
 
 model = Wav2Vec2ForCTC.from_pretrained(
     model_path,
-    # attention_dropout=0.1,
-    # hidden_dropout=0.1,
-    # feat_proj_dropout=0.0,
-    # mask_time_prob=0.05,
-    # layerdrop=0.1,
-    attention_dropout=0.3,
-    hidden_dropout=0.3,
-    feat_proj_dropout=0.2,
-    mask_time_prob=0.25,
-    layerdrop=0.3,
+    attention_dropout=0.05,
+    hidden_dropout=0.05,
+    feat_proj_dropout=0.0,
+    mask_time_prob=0.01,
+    layerdrop=0.05,
     ctc_loss_reduction='mean', 
     pad_token_id=processor.tokenizer.pad_token_id,
     vocab_size=processor.tokenizer.vocab_size
